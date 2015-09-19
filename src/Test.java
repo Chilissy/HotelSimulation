a515d828d5c86cf2c0bb744f77358655883d7d8f
Chilissy <al3x.gold3n@gmail.com>
2015-09-19 22:43:15 +0200

    blabla2

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,18 @@
+import model.Reservation;
+import model.Room;
 
-public class Test 
+import java.time.LocalDate;
+
+public class Test
 {
 	public static void main(String args[])
 	{
 		System.out.println("Test");
 		System.out.println("axaxa");
-	
+
+		Room room = new Room(100, 1, 2, 3);
+
+		Reservation reservation = new Reservation(room, LocalDate.now(), LocalDate.now().plusDays(2));
 	}
 
 }
